530b41d8aaf6661f6396dfb3b60e20f7e0950740
Greg Kroah-Hartman <gregkh@google.com>
2020-03-10 18:39:57 +0100

    ANDROID: GKI: sched.h: add Android ABI padding to some structures
    Try to mitigate potential future driver core api changes by adding a
    pointer to struct signal_struct, struct sched_entity, struct
    sched_rt_entity, and struct task_struct.
    
    Based on a patch from Michal Marek <mmarek@suse.cz> from the SLES kernel
    
    Bug: 151154716
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I1449735b836399e9b356608727092334daf5c36b
    (cherry picked from commit b14ffb0250b8ee00abd790ff616cfed7da8916f6)

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -29,6 +29,7 @@
 #include <linux/mm_event.h>
 #include <linux/task_io_accounting.h>
 #include <linux/rseq.h>
+#include <linux/android_kabi.h>
 
 /* task_struct member predeclarations (sorted alphabetically): */
 struct audit_context;
@@ -559,6 +560,11 @@ struct sched_entity {
 	 */
 	struct sched_avg		avg;
 #endif
+
+	ANDROID_KABI_RESERVE(1);
+	ANDROID_KABI_RESERVE(2);
+	ANDROID_KABI_RESERVE(3);
+	ANDROID_KABI_RESERVE(4);
 };
 
 struct sched_load {
@@ -677,6 +683,11 @@ struct sched_rt_entity {
 	/* rq "owned" by this entity/group: */
 	struct rt_rq			*my_q;
 #endif
+
+	ANDROID_KABI_RESERVE(1);
+	ANDROID_KABI_RESERVE(2);
+	ANDROID_KABI_RESERVE(3);
+	ANDROID_KABI_RESERVE(4);
 } __randomize_layout;
 
 struct sched_dl_entity {
