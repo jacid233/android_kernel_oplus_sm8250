468651743f4739e27a277dbee4f3f1ae8648a66b
Lucas Wei <lucaswei@google.com>
2021-07-30 15:40:20 +0800

    Revert "usb: dwc3: debugfs: Add and remove endpoint dirs dynamically"
    This reverts commit 3bef21035888dd19750ad79b5d08fe52a02ad719.
    
    Reason for revert: Boot fail with this patch.
    Bug: 194984062
    Signed-off-by: Lucas Wei <lucaswei@google.com>
    Change-Id: Ice1c308f6ff0255a5f541a6351ceebcda6b3ddca

--- drivers/usb/dwc3/debugfs.c
+++ drivers/usb/dwc3/debugfs.c
@@ -819,11 +819,12 @@ static void dwc3_debugfs_create_endpoint_files(struct dwc3_ep *dep,
 	}
 }
 
-void dwc3_debugfs_create_endpoint_dir(struct dwc3_ep *dep)
+static void dwc3_debugfs_create_endpoint_dir(struct dwc3_ep *dep,
+		struct dentry *parent)
 {
 	struct dentry		*dir;
 
-	dir = debugfs_create_dir(dep->name, dep->dwc->root);
+	dir = debugfs_create_dir(dep->name, parent);
 	if (!dir) {
 		pr_err("%s: failed to create dir %s\n", __func__, dep->name);
 		return;
@@ -1055,6 +1056,7 @@ void dwc3_debugfs_init(struct dwc3 *dwc)
 				    &dwc3_testmode_fops);
 		debugfs_create_file("link_state", S_IRUGO | S_IWUSR, root, dwc,
 				    &dwc3_link_state_fops);
+		dwc3_debugfs_create_endpoint_dirs(dwc, root);
 
 		file = debugfs_create_file("int_events", 0644, root, dwc,
 				&dwc3_gadget_dbg_events_fops);
