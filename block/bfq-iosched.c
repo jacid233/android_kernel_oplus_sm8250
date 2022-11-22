c61b8d7893837b7c537d4f87add9bb5cfa09e813
Michael Bestas <mkbestas@lineageos.org>
2022-11-22 20:34:43 +0200

    Revert "block: use current active bfqq to update statistics"
    Revert to reduce upstream merge conflicts.
    
    This reverts commit 1852d0be8d78f77ee73527ccff73ace073267851.
    
    Change-Id: I41def223623912099d3b35c3693b297762419eec

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -4080,7 +4080,7 @@ exit:
 #if defined(CONFIG_BFQ_GROUP_IOSCHED) && defined(CONFIG_DEBUG_BLK_CGROUP)
 static void bfq_update_dispatch_stats(struct request_queue *q,
 				      struct request *rq,
-					  struct bfq_queue *in_serv_queue,
+				      struct bfq_queue *in_serv_queue,
 				      bool idle_timer_disabled)
 {
 	struct bfq_queue *bfqq = rq ? RQ_BFQQ(rq) : NULL;
@@ -4125,7 +4125,7 @@ static void bfq_update_dispatch_stats(struct request_queue *q,
 #else
 static inline void bfq_update_dispatch_stats(struct request_queue *q,
 					     struct request *rq,
-						 struct bfq_queue *in_serv_queue,
+					     struct bfq_queue *in_serv_queue,
 					     bool idle_timer_disabled) {}
 #endif
 
