15ed7d4e307a25e1843112e574801546e7dcb5ec
Mashopy <eliasgheeraert@gmail.com>
2023-03-26 00:55:04 +0100

    power: oplus_battery_msm8250: Fix estimated charging time
    Super VOOC charging technology estimation charging time is stuck at 1 minute without this change
    
    Been broken during since 85bebcb92af7dca684f347bdb43dbe5d014658c8
    
    Change-Id: I62847b85019bd05db8366e636dd2ef1e59031a18

--- drivers/power/oplus/charger_ic/oplus_battery_msm8250.c
+++ drivers/power/oplus/charger_ic/oplus_battery_msm8250.c
@@ -14429,8 +14429,10 @@ static enum power_supply_property smb5_batt_props[] = {
 	POWER_SUPPLY_PROP_RECHARGE_SOC,
 	POWER_SUPPLY_PROP_CHARGE_FULL,
 	POWER_SUPPLY_PROP_FORCE_RECHARGE,
+#ifndef OPLUS_FEATURE_CHG_BASIC
 	POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN,
 	POWER_SUPPLY_PROP_TIME_TO_FULL_NOW,
+#endif
 	POWER_SUPPLY_PROP_FCC_STEPPER_ENABLE,
 #ifdef OPLUS_FEATURE_CHG_BASIC
 	POWER_SUPPLY_PROP_VOLTAGE_MIN,
@@ -14615,6 +14617,7 @@ static int smb5_batt_get_prop(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_FORCE_RECHARGE:
 		val->intval = 0;
 		break;
+#ifndef OPLUS_FEATURE_CHG_BASIC
 	case POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN:
 		rc = smblib_get_prop_from_bms(chg,
 				POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN, val);
@@ -14623,6 +14626,7 @@ static int smb5_batt_get_prop(struct power_supply *psy,
 		rc = smblib_get_prop_from_bms(chg,
 				POWER_SUPPLY_PROP_TIME_TO_FULL_NOW, val);
 		break;
+#endif
 	case POWER_SUPPLY_PROP_FCC_STEPPER_ENABLE:
 		val->intval = chg->fcc_stepper_enable;
 		break;
