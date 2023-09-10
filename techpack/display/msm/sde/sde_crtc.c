e60eb3c8154a2e4affa219962b82487e7bb6ec52
Mashopy <eliasgheeraert@gmail.com>
2023-09-10 06:06:09 +0200

    Revert "techpack: display: Do not configure the fp dim layer on low-power mode"
    This reverts commit 96d32b5372da2e1b40ad5beaaa7ee6a848c3fa73.

--- techpack/display/msm/sde/sde_crtc.c
+++ techpack/display/msm/sde/sde_crtc.c
@@ -4794,8 +4794,7 @@ static int sde_crtc_onscreenfinger_atomic_check(struct sde_crtc_state *cstate,
 	cstate->fingerprint_pressed = false;
 
 	/* initalize dim layer */
-	panel_power_mode = oplus_get_panel_power_mode();
-	if ((dimlayer_hbm || dimlayer_bl) && (panel_power_mode != SDE_MODE_DPMS_LP1) && (panel_power_mode != SDE_MODE_DPMS_LP2)) {
+	if (dimlayer_hbm || dimlayer_bl) {
 		if (fp_index >= 0 && fppressed_index >= 0) {
 			if (pstates[fp_index].stage >= pstates[fppressed_index].stage) {
 				SDE_ERROR("Bug!!: fp layer top of fppressed layer\n");
@@ -4831,6 +4830,7 @@ static int sde_crtc_onscreenfinger_atomic_check(struct sde_crtc_state *cstate,
 			cstate->fingerprint_mode = false;
 
 		SDE_DEBUG("debug for get cstate->fingerprint_mode = %d\n", cstate->fingerprint_mode);
+		panel_power_mode = oplus_get_panel_power_mode();
 
 		/* when aod layer is present */
 		if (aod_index >= 0) {
